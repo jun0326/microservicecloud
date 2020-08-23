80a99c680a5d0645bcafdd06832c66696f9735cd
jun0326 <15766228041@163.com>
2020-08-23 21:01:22 +0800

    test branch jun_dev

--- microservicecloud-consumer-dept-80/src/main/java/cn/jun/springcloud/controller/TestController.java
+++ microservicecloud-consumer-dept-80/src/main/java/cn/jun/springcloud/controller/TestController.java
@@ -3,6 +3,6 @@ package cn.jun.springcloud.controller;
 public class TestController {
 
     public static void main(String[] args) {
-        System.out.println("-------");
+        System.out.println("----好的---");
     }
 }
